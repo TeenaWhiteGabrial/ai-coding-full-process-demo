45e1bc973579175eccb420431d32dc4bc399aaa2
TeenaWhiteGabrial <2386080939@qq.com>
2026-06-08 10:00:12 +0800

    chore: commit remaining staged changes

--- code/webs/manage-web-demo/vite.config.ts
+++ code/webs/manage-web-demo/vite.config.ts
@@ -28,15 +28,15 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/ai-studio/v1': {
-         target: 'http://localhost:8080',
+         target: 'http://localhost:18081',
         // target: 'http://39.105.212.130',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:18081',
         // target: 'http://39.105.212.130',
         changeOrigin: true,
-      },
+      },  
     },
   },
 })
